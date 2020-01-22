13f390783d736a78f90889b714f92313d121be7b
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-22 23:29:01 +0100

    added footer

--- css/js/main.js
+++ css/js/main.js
@@ -1,21 +1,23 @@
 
-//    function initMap() {
-//         map = new google.maps.Map(document.getElementById('map'), {
-//           center: {lat: 54.566921, lng: 18.399099},
-//           zoom: 8
-//         });
-//       }
+         var map;
+
+   function initMap() {
+        map = new google.maps.Map(document.getElementByClassName('map'), {
+          center: {lat: 54.566921, lng: 18.399099},
+          zoom: 8
+        });
+      }
 
 
-function initMap(){
-    const loc={lat: 54.566921, lng: 18.399099};
-    const map=new google.maps.Map(document.querySelector('.map'),
-    {
-        zoom: 14,
-        center: loc
-    });
-    const marker = new google.maps.Marker({
-        position: loc,
-        map: map
-    })
-}
+// function initMap(){
+//     const loc={lat: 54.566921, lng: 18.399099};
+//     const map=new google.maps.Map(document.querySelector('.map'),
+//     {
+//         zoom: 14,
+//         center: loc
+//     });
+//     const marker = new google.maps.Marker({
+//         position: loc,
+//         map: map
+//     })
+// }
