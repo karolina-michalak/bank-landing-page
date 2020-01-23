03a08f1bb93105eb7c47c9d6e68900a730ae0ece
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-23 10:02:23 +0100

    added transparent navbar function

--- js/main.js
+++ js/main.js
@@ -10,3 +10,11 @@ $('#navbar a, .btn').on('click', function(event){
       )
     }
 })
+
+window.addEventListener('scroll', function(){
+  if(window.scrollY > 150){
+    document.querySelector('#navbar').style.opacity = 0.95
+  } else{
+    document.querySelector('#navbar').style.opacity = 1
+  }
+})
