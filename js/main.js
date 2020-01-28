8254d4097bc725e7b2d08acd1d6fca766f2c862b
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-28 17:30:44 +0100

    trying to fix highlighting

--- js/main.js
+++ js/main.js
@@ -4,18 +4,21 @@ window.addEventListener('scroll', function () {
   const navButtons = document.getElementsByClassName('colored');
   const about = document.getElementById('about');
   const team = document.getElementById('team');
+  const clients = document.getElementById('clients');
   const contact = document.getElementById('contact');
 
-for(let i=0; i < navButtons.length; i++){
-  navButtons[i].classList.remove('active')
-}
-  if(currentPosition >= about.offsetTop && currentPosition < team.offsetTop){
-    navButtons[0].classList.add('active')
-  } else if(currentPosition >= team.offsetTop && currentPosition < contact.offsetTop){
-    navButtons[1].classList.add('active')
-  } else if(currentPosition >= contact.offsetTop){
-    navButtons[2].classList.add('active')
+  for(let i=0; i < navButtons.length; i++){
+    navButtons[i].classList.remove('active')
   }
+    if(currentPosition >= about.offsetTop && currentPosition < team.offsetTop){
+      navButtons[0].classList.add('active')
+    } else if(currentPosition >= team.offsetTop && currentPosition < clients.offsetTop){
+      navButtons[1].classList.add('active')
+    } else if(currentPosition >= clients.offsetTop && currentPosition < contact.offsetTop){
+      navButtons[2].classList.add('active')
+    } else if(currentPosition >= contact.offsetTop){
+      navButtons[3].classList.add('active')
+    }
 
 })
 
