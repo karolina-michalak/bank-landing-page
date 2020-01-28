a7f282502a6767e16ee55b3245c1292e52df7a8b
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-28 17:14:56 +0100

    highlighting nav buttons when scrolling

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-$('#navbar a, .btn').on('click', function(event){
-    if(this.hash !== ''){
-      event.preventDefault();
-      const hash = this.hash;
-      $('html, body').animate(
-        {
-          scrollTop: $(hash).offset().top - 80
-        },
-        700
-      )
-    }
+window.addEventListener('scroll', function () {
+
+  const currentPosition = window.scrollY + window.outerHeight / 2;
+  const navButtons = document.getElementsByClassName('colored');
+  const about = document.getElementById('about');
+  const team = document.getElementById('team');
+  const contact = document.getElementById('contact');
+
+for(let i=0; i < navButtons.length; i++){
+  navButtons[i].classList.remove('active')
+}
+  if(currentPosition >= about.offsetTop && currentPosition < team.offsetTop){
+    navButtons[0].classList.add('active')
+  } else if(currentPosition >= team.offsetTop && currentPosition < contact.offsetTop){
+    navButtons[1].classList.add('active')
+  } else if(currentPosition >= contact.offsetTop){
+    navButtons[2].classList.add('active')
+  }
+
 })
 
-window.addEventListener('scroll', function(){
-  if(window.scrollY > 150){
-    document.querySelector('#navbar').style.opacity = 0.95
-  } else{
-    document.querySelector('#navbar').style.opacity = 1
-  }
-})
+
