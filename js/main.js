4629b1f7528f71d27ccad5150fbe4f9bd3ef7a69
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-05 13:44:20 +0100

    changed logo color and added toTheTop button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('scroll', function () {
 
   const currentPosition = document.documentElement.scrollTop + 110;
@@ -8,20 +7,32 @@ window.addEventListener('scroll', function () {
   const clients = document.getElementById('clients');
   const contact = document.getElementById('contact');
 
-  for(let i=0; i < navButtons.length; i++){
+  for (let i = 0; i < navButtons.length; i++) {
     navButtons[i].classList.remove('active')
   }
-    if(currentPosition >= about.offsetTop && currentPosition < team.offsetTop){
-      navButtons[0].classList.add('active')
-    } else if(currentPosition >= team.offsetTop && currentPosition < clients.offsetTop){
-      navButtons[1].classList.add('active')
-    } else if(currentPosition >= clients.offsetTop && currentPosition < contact.offsetTop){
-      navButtons[2].classList.add('active')
-    } else if(currentPosition >= contact.offsetTop){
-      navButtons[3].classList.add('active')
-    }
+  if (currentPosition >= about.offsetTop && currentPosition < team.offsetTop) {
+    navButtons[0].classList.add('active')
+  } else if (currentPosition >= team.offsetTop && currentPosition < clients.offsetTop) {
+    navButtons[1].classList.add('active')
+  } else if (currentPosition >= clients.offsetTop && currentPosition < contact.offsetTop) {
+    navButtons[2].classList.add('active')
+  } else if (currentPosition >= contact.offsetTop) {
+    navButtons[3].classList.add('active')
+  }
 
 })
 
 
+const toTheTopButton = document.querySelector('.toTheTopButton');
 
+window.addEventListener('scroll', function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTheTopButton.style.display = "block";
+  } else {
+    toTheTopButton.style.display = "none";
+  }
+});
+
+toTheTopButton.addEventListener('click', function () {
+  document.documentElement.scrollTop = 0;
+})
